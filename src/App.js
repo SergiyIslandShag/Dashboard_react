b565949e0a554e826ff85af8626ac3d25e4941aa
SergiyIslandShag <sergiykoziar13@gmail.com>
2025-01-10 18:37:22 +0200

    intro

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="App">
+        <h1>Dashboard projects</h1>
+      </div>
+      <div className="content">
+        <p>Content text</p>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
