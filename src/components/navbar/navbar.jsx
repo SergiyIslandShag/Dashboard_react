1a421c2f4ca0f21d65687dbe1f389a707ac82642
SergiySergiyIslandShag <sergiykoziar13@gmail.com>
2025-01-17 20:25:06 +0200

    fix

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,9 @@
-import { SolidButton } from "../../Buttons";
 import * as style from "./style";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
 import "./style.css";
+import { IconButton, Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ isDark = false, themeCallback }) => {
     const navLinkStyle = isDark ? style.navLinkDark : style.navLinkLight;
@@ -10,48 +13,39 @@ const Navbar = ({ isDark = false, themeCallback }) => {
             style={isDark ? style.navbarDark : style.navbarLight}
             className="navbar"
         >
-            <a style={navLinkStyle} href="#">
-                Main page
-            </a>
-            <a style={navLinkStyle} href="#">
-                About
-            </a>
-            <a style={navLinkStyle} href="#">
-                Page 3
-            </a>
-            <a style={navLinkStyle} href="#">
-                Page 4
-            </a>
-            <SolidButton text="Page 5" color="green" />
-            <div onClick={themeCallback}>
-                <svg
-                    width="32px"
-                    height="32px"
-                    viewBox="0 0 24 24"
-                    version="1.1"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <title>ic_fluent_dark_theme_24_regular</title>
-                    <desc>Created with Sketch.</desc>
-                    <g
-                        id="🔍-Product-Icons"
-                        stroke="none"
-                        strokeWidth="1"
-                        fill="none"
-                        fillRule="evenodd"
-                    >
-                        <g
-                            id="ic_fluent_dark_theme_24_regular"
-                            fill={isDark ? "#fff" : "#212121"}
-                            fillRule="nonzero"
-                        >
-                            <path
-                                d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z"
-                                id="🎨-Color"
-                            ></path>
-                        </g>
-                    </g>
-                </svg>
+            <div className="navlinks">
+                <Link style={navLinkStyle} to="/">
+                    Main page
+                </Link>
+                <Link style={navLinkStyle} to="/about">
+                    About
+                </Link>
+                <Link style={navLinkStyle} to="/users">
+                    Users
+                </Link>
+                <Link style={navLinkStyle} to="/">
+                    Page 4
+                </Link>
+            </div>
+
+            <div onClick={themeCallback} className="navtheme">
+                {isDark ? (
+                    <IconButton sx={{ color: "white" }}>
+                        <LightModeIcon />
+                    </IconButton>
+                ) : (
+                    <IconButton>
+                        <DarkModeIcon />
+                    </IconButton>
+                )}
+            </div>
+            <div className="navauth">
+                <Link to="login">
+                    <Button style={{margin: "0px 5px 0px 0px"}} variant="contained">Login</Button>
+                </Link>
+                <Link to="register">
+                    <Button style={{margin: "0px 10px 0px 5px"}} variant="contained">Register</Button>
+                </Link>
             </div>
         </div>
     );
---
+++ src/pages/mainPage/mainPage.js
@@ -0,0 +1,9 @@
+import "./MainPage.css";
+
+const MainPage = () => {
+    return (
+        <img style={{marginTop: "8px", width: "100%"}} src="https://img.freepik.com/premium-vector/main-web-page-design_87720-407.jpg"/>
+    );
+};
+
+export default MainPage;
